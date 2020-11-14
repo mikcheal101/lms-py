1293928d8e91345396d18a0850ab94b313e0a09a
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-14 15:44:24 +0100

    hemen/Issue model created

--- lms/book_management/models.py
+++ lms/book_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User as Customer
 
 
 class Book(models.Model):
@@ -11,6 +12,36 @@ class Book(models.Model):
 
     def __repr__(self):
         return self.name
+
+    class Meta:
+        ordering = ('id', )
+
+
+class Issue(models.Model):
+    """
+    Issue Model
+    table holding the book's issue
+    """
+
+    customer = models.ForeignKey(
+        to=Customer,
+        related_name='issues',
+        on_delete=models.CASCADE)
+
+    book = models.ForeignKey(
+        to=Book,
+        related_name='issues',
+        on_delete=models.CASCADE)
+
+    issue_date = models.DateField()
+    return_date = models.DateField()
+
+    period = models.IntegerField(default=1)
+    fine = models.FloatField(default=100.00)
+    """ fine per day """
+
+    def __repr__(self):
+        pass
 
     class Meta:
         ordering = ('id', )
