760bda33b8d8b02c07d10a3fb7c4930937eb9ba2
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-14 15:37:46 +0100

    hemen/Book created

--- lms/book_management/models.py
+++ lms/book_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """
+    Book Model
+    """
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+    genre = models.CharField(max_length=200)
+
+    def __repr__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id', )
