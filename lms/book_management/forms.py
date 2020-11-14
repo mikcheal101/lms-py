483bcc8e62a8d73ceee29cc218551cdd0095d49f
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-14 16:45:30 +0100

    hemen/book management forms created

---
+++ lms/book_management/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from book_management.models import Book, Issue
+from django.contrib.auth.models import User as Staff
+from django.contrib.auth.models import User as Customer
+
+
+class IssueForm(forms.ModelForm):
+
+    customer = forms.ModelChoiceField(
+        queryset=Customer.objects.filter(is_staff=False))
+
+    staff = forms.ModelChoiceField(
+        queryset=Staff.objects.filter(is_staff=True))
+
+    book = forms.ModelChoiceField(
+        queryset=Book.objects.all())
+
+    class Meta:
+        model = Issue
+        fields = [
+            'issue_date',
+            'expected_date',
+            'return_date',
+            'fine',
+            'customer_id',
+            'customer',
+            'book',
+            'book_id', ]
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'price', 'genre']
